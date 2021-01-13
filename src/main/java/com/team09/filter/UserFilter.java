31904c7850e2c4700f8c1f8b4ba0b62e28c59d7e
997Yi <775242373@qq.com>
2021-01-13 08:44:52 +0800

    UserFilter

---
+++ src/main/java/com/team09/filter/UserFilter.java
@@ -0,0 +1,27 @@
+package com.team09.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/user/delBlog", "/user/delComment", "/user/modifyUser",
+        "/user/postBlog", "/user/PostCommentServlet", "/view/postBlog.jsp"})
+public class UserFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (httpServletRequest.getSession().getAttribute("userInfo") != null) {
+            chain.doFilter(request, response);
+        }else {
+            httpServletRequest.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
+}
