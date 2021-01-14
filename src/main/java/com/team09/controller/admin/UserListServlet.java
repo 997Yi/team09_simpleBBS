9c8aa2767e85c897d843b7cc62f266781ff6a42d
WangWeiQing <wang.weiqing@qq.com>
2021-01-14 17:28:20 +0800

    添加fastjson依赖以及将对象转换成json发送给页面

--- src/main/java/com/team09/controller/admin/UserListServlet.java
+++ src/main/java/com/team09/controller/admin/UserListServlet.java
@@ -1,5 +1,6 @@
 package com.team09.controller.admin;
 
+import com.alibaba.fastjson.JSON;
 import com.team09.bean.User;
 import com.team09.service.UserService;
 import com.team09.service.impl.UserServiceImpl;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/admin/list")
@@ -25,8 +27,12 @@ public class UserListServlet extends HttpServlet {
 
         UserService userService=UserServiceImpl.getInstance();
         List<User> userList=userService.findByPage(page,pageSize);
-        req.setAttribute("userList",userList);
-        req.getRequestDispatcher("../userList.jsp").forward(req,resp);
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String str= JSON.toJSONString(userList);//使用fastjson
+        PrintWriter out=resp.getWriter();
+        out.print("{\"code\":0 , \"count\":"+ 100 +" , \"data\":"+str+"}");
     }
 
 
